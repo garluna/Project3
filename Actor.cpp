7d9c7cb081441742dd55cb6a096798e1d52410dd
garluna <garluna@users.noreply.github.com>
2015-02-18 13:33:32 -0800

    Create Actor.cpp

---
+++ Actor.cpp
@@ -0,0 +1,10 @@
+#include "Actor.h"
+#include "StudentWorld.h"
+
+// Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
+//implementation of base, Player, Wall
+
+Actor::Actor()
+{
+	setVisible(true);
+}
