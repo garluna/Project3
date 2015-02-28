17bc19db74bd56071fa61ef2c915bcd515aa868d
garluna <garluna@users.noreply.github.com>
2015-02-28 02:46:59 -0800

    Update StudentWorld.cpp

--- StudentWorld.cpp
+++ StudentWorld.cpp
@@ -13,20 +13,39 @@ GameWorld* createStudentWorld(string assetDir)
 
 int StudentWorld::init()
 {
-	loadLevel();
-	return GWSTATUS_CONTINUE_GAME;
+	if (loadLevel() == -1)
+	{
+		return GWSTATUS_PLAYER_WON;
+	}
+	else
+	{
+		return GWSTATUS_CONTINUE_GAME;
+	}
 }
 
 int StudentWorld::move()
 {
-	// This code is here merely to allow the game to build, run, and terminate after hitting enter a few times 
+	setDisplayText();
+	if (!currentPlayer->isAlive())
+	{
+		decLives();
+		return GWSTATUS_PLAYER_DIED;
+	}
+	if (bonus > 0)
+	{
+		bonus--;
+	}
 	currentPlayer->doSomething();
 	for (int i = 0; i < actors.size(); i++)
 	{
-		if (actors[i]->isAlive())
+		if (!(actors[i]->isAlive()))
 		{
-			actors[i]->doSomething();
+			removeActor(actors[i]);
 		}
+	}
+	for (int i = 0; i < actors.size(); i++)
+	{
+		actors[i]->doSomething();
 	}
 	/*int count = 0;  //CHECK - use iterator???
 	for (int i = 0; i < actors.size(); i++)
@@ -36,6 +55,16 @@ int StudentWorld::move()
 			actors[i] = actors[actors.size() - 1];
 		}
 	}*/
+	if (getLives() == 0)
+	{
+		return GWSTATUS_PLAYER_DIED;  //CHECK
+	}
+	if (exitUsed)
+	{
+		increaseScore(2000 + bonus);
+		setExitUsed(false);
+		return GWSTATUS_FINISHED_LEVEL;
+	}
 	return GWSTATUS_CONTINUE_GAME;
 	/*
  // Give each actor a chance to do something
@@ -133,6 +162,7 @@ int StudentWorld::loadLevel()
 				break;
 			case Level::jewel:
 				actors.push_back(new Jewel(IID_JEWEL, x, y, this));
+				numJewels++;
 				break;
 			case Level::extra_life:
 				actors.push_back(new ExtraLife(IID_EXTRA_LIFE, x, y, this));
@@ -149,6 +179,9 @@ int StudentWorld::loadLevel()
 			case Level::hole:
 				actors.push_back(new Hole(IID_HOLE, x, y, this));
 				break;
+			case Level::exit:
+				actors.push_back(new Exit(IID_EXIT, x, y, this));
+				break;
 			}
 		}
 	}
@@ -159,12 +192,34 @@ Level* StudentWorld::getLevelPointer() const
 {
 	return lev;
 }
-Player* StudentWorld::returnCurrentPlayer()
+Player* StudentWorld::returnCurrentPlayer() const
 {
 	return currentPlayer;
 }
 
-Actor* StudentWorld::getActorsAtLoc(int x, int y)
+void StudentWorld::removeActor(Actor* a)
+{
+	vector<Actor*>::iterator it;
+	it = actors.begin();
+	while (it != actors.end())
+	{
+		if (*it == a)
+		{
+			//cerr << "remove\n";
+			Actor* temp;
+			temp = *it;
+			it = actors.erase(it);
+			delete temp;
+			return;
+		}
+		else
+		{
+			it++;
+		}
+	}
+}
+
+Actor* StudentWorld::getActorsAtLoc(int x, int y) const
 {
 	for (int i = 0; i < actors.size(); i++)
 	{
@@ -175,4 +230,84 @@ Actor* StudentWorld::getActorsAtLoc(int x, int y)
 	}
 	return nullptr;
 }
+
+int StudentWorld::getCurrentScore() const
+{
+	return getScore();
+}
+
+int StudentWorld::getCurrentGameLevel() const
+{
+	return getLevel();
+}
+
+void StudentWorld::setDisplayText() 
+{
+	int score = getScore();
+	int level = getLevel();
+	int livesLeft = getLives();
+
+	string display = format(score, level, livesLeft, currentPlayer->getHitPoints(), currentPlayer->getAmmo(), bonus);
+
+	setGameStatText(display);
+}
+
+string StudentWorld::format(int score, int lvl, int lives, int health, int ammo, int bonus)
+{
+	ostringstream scoreDisplay;
+	scoreDisplay << "Score: ";
+	scoreDisplay.fill('0');
+	scoreDisplay << setw(7) << score << "  ";
+
+	ostringstream levelDisplay;
+	levelDisplay << "Level: ";
+	levelDisplay.fill('0');
+	levelDisplay << setw(2) << lvl << "  ";
+
+	ostringstream livesDisplay;
+	livesDisplay << "Lives: ";
+	livesDisplay.fill(' ');
+	livesDisplay << setw(2) << lives << "  ";
+
+	health *= 5;
+	ostringstream healthDisplay;
+	healthDisplay << "Health: ";
+	healthDisplay.fill(' ');
+	healthDisplay << setw(3) << health << "%  ";
+
+	ostringstream ammoDisplay;
+	ammoDisplay << "Ammo: ";
+	ammoDisplay.fill(' ');
+	ammoDisplay << setw(3) << ammo << "  ";
+
+	ostringstream bonusDisplay;
+	bonusDisplay << "Bonus: ";
+	bonusDisplay.fill(' ');
+	bonusDisplay << setw(4) << bonus << "  ";
+
+	string toDisplay;
+	toDisplay = scoreDisplay.str() + levelDisplay.str() + livesDisplay.str() + healthDisplay.str() + ammoDisplay.str() + bonusDisplay.str();
+	return toDisplay;
+}
+
+int StudentWorld::getJewelNum() const
+{
+	return numJewels;
+}
+
+void StudentWorld::decJewels()
+{
+	numJewels--;
+}
+
+void StudentWorld::setExitUsed(bool a)
+{
+	exitUsed = a;
+}
+
+void StudentWorld::addBullet(int x, int y, GraphObject::Direction dir)
+{
+	actors.push_back(new Bullet(IID_BULLET, x, y, dir, this));
+}
+
 // Students:  Add code to this file (if you wish), StudentWorld.h, Actor.h and Actor.cpp
